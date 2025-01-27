171824a15fde4a9b4925357553dd199fed45aca3
Ayush jha <jhaa97204@gmail.com>
2025-01-27 19:47:00 +0530

    variables

---
+++ Javascript/Variables/variables.js
@@ -0,0 +1,10 @@
+const accountId = 122233
+let accountEmail = "ayush@gmail.com"
+var accountPassword = "12345"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+
+
