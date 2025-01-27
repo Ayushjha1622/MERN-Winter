83cc8f44cb94f33a30d1205f1248d65db01b07c3
Ayush jha <jhaa97204@gmail.com>
2025-01-27 21:26:39 +0530

    new file

---
+++ Javascript/String/main.js
@@ -0,0 +1,47 @@
+const name = "ayush"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('ayushjha')
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('j'));
+
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+
+
+const newStringOne = "      ayush      "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('hitesh'));
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
