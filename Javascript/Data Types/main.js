ae73fdbd3f98cf572160a971cea631879a4b99f4
Ayush jha <jhaa97204@gmail.com>
2025-01-27 20:16:59 +0530

    data types conversion

---
+++ Javascript/Data Types/main.js
@@ -0,0 +1,34 @@
+// alert(3+3)  // we r usinh node js,not browser
+
+let score = 69
+
+console.log(typeof(score));
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase => 0
+
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
--- Javascript/Variables/variables.js
+++ Javascript/Variables/variables.js
@@ -3,8 +3,10 @@ let accountEmail = "ayush@gmail.com"
 var accountPassword = "12345"
 
 
-console.log(accountId);
-console.log(accountEmail);
-console.log(accountPassword);
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+console.table([accountId,accountEmail,accountPassword])
 
 
