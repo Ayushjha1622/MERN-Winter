2aaa0658228d64a3e894a0f4c4518f6e70404752
Ayush jha <jhaa97204@gmail.com>
2025-01-27 21:02:21 +0530

    new file

---
+++ Javascript/stack/main.js
@@ -0,0 +1,19 @@
+// Stack (primitive) , heap(Non-Primitive)
+
+let myName = "ayush@gmail.com"
+anothername = "chaiaurcode"
+
+console.log(myName);
+console.log(anothername);
+
+
+let userOne = {
+    email: "abc@gmail.com",
+    upi: "user@ybl"
+}
+
+
+let userTwo = userOne
+userTwo.email = "ayush@gmail.com"
+
+
