e91ac18799b63f601e0cbd2116f6dfff184622ce
Ayush jha <jhaa97204@gmail.com>
2025-01-28 19:05:39 +0530

    new file

---
+++ Javascript/Basics_03/Hoisting/main.js
@@ -0,0 +1,41 @@
+// function one() {
+//     const username = "hitesh"
+
+//         function two() {
+//             const website = "youtube"
+//                 // console.log(website);
+            
+//         }
+
+//         console.log(website);
+//         two()
+        
+// }
+
+// one()
+
+
+
+if (true) {
+    const username = "ayush"
+    if (username === "ayush") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+
+// ++++++++++++++++++++++++  interesting ++++++++++++++++++++++++
+
+function addOne(num){
+    return num + 1;
+
+}
+
+addOne(5)
