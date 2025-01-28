fc191ebd60742bb16b6bf9cbd877dff15ad44bcb
Ayush jha <jhaa97204@gmail.com>
2025-01-28 18:34:32 +0530

    new file

---
+++ Javascript/Basics_03/functions/main.js
@@ -0,0 +1,48 @@
+// function sayMyName() {
+//     console.log("A");
+//     console.log("A");
+//     console.log("A");
+//     console.log("A");
+//     console.log("A");
+//     console.log("A");
+    
+
+// }
+
+// sayMyName()
+
+
+
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1+number2);
+// }
+
+// addTwoNumbers(3,4)
+
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1+number2);
+// }
+
+// function addTwoNumbers(number1, number2){
+//     let result = number1 + number2
+//     return result
+// }
+
+// const result = addTwoNumbers(3,5)
+// console.log("Result: ",result);
+
+
+
+function loginUserMessage(username) {
+    if (username === undefined) {
+        console.log("please enter a username");
+        
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("ayush"))
+
+
