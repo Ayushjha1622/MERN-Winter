a04e2df5a93bd2c2a3b5d407db0db264bfe414c1
Ayush jha <jhaa97204@gmail.com>
2025-01-28 11:24:26 +0530

    new file

---
+++ Javascript/Basics_02/main2.js
@@ -0,0 +1,52 @@
+// const tinderUser = new Object()    // singleton object
+
+const tinderUser = {}   // non singleton
+
+tinderUser.id = "123avbc"
+tinderUser.name = "John Doe"
+tinderUser.age = 30
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "ayush",
+            lastname: "jha"
+        }
+    }
+}
+
+// console.log(regularUser.fullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+// const obj3 = Object.assign({},obj1, obj2)
+// console.log(obj3);
+
+
+const obj3 = {...obj1, ...obj3}
+console.log(obj3);
+
+
+
+// const obj3 = { obj1, obj2}
+// console.log(obj3);
+
+const users =[
+    {
+        id: 1,
+        email: "g@gmail.com"
+    }
+] 
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
