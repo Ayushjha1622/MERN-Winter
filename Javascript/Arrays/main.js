1203fadf7d541268332e4bc8a856082a1a3f00b6
Ayush jha <jhaa97204@gmail.com>
2025-01-28 10:09:39 +0530

    new file

---
+++ Javascript/Arrays/main.js
@@ -0,0 +1,63 @@
+// array
+
+const myArray = [0,1,2,3,4,5,6]
+const myHeroes = ["iron","super"]
+
+const myArr = new Array(0,1,2,3,4,5,6)
+// console.log(myArray[0]);
+// console.log(myArr);
+
+
+// Array methods
+
+// myArr.push(8)
+// myArr.push(7)
+// myArr.push(9)
+// console.log(myArr);
+
+// myArr.pop(9)
+// console.log(myArr);
+
+
+// myArr.unshift(9)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(19));
+// console.log(myArr);
+
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+
+//  slice , splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C",myArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
