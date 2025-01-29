bcff44df3c10db567aa095bc8f0852002fd2faa5
Ayush jha <jhaa97204@gmail.com>
2025-01-29 10:49:47 +0530

    new file

---
+++ Javascript/Loops/while.js
@@ -0,0 +1,16 @@
+let index = 0
+
+while (index<=10) {
+    index =index + 2
+    console.log(index);
+    
+} 
+
+
+let score = 1
+
+do{
+    console.log(`Score is ${score}`);
+    score = score + 1
+    
+} while(score <= 10);
