42c884f57abc38f7e51835675233179200023c03
Ayush jha <jhaa97204@gmail.com>
2025-01-29 15:00:42 +0530

    new file

---
+++ Javascript/High order Loops/main.js
@@ -0,0 +1,29 @@
+// const arr = [1,2,3,4,5]
+
+// for(const num of object){
+//     // console.log(num);
+    
+// }
+
+
+
+// const greetings = "hello world!"
+// for(const greet of greetings){
+//     // console.log(`Each char is ${greet}`);
+    
+// }
+
+
+// Maps
+
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('UK',"United Kingdom")
+// console.log(map);
+
+
+for(const [key, value] of map){
+    console.log(key, ':-', value);
+}
