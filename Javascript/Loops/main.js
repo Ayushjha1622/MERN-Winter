0169b9b7568fa7673ef9e98d9975586e3751e2b2
Ayush jha <jhaa97204@gmail.com>
2025-01-29 10:45:35 +0530

    new file

---
+++ Javascript/Loops/main.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 10; i++) {
+        const element = i;
+        if ( element==5 ) {
+            // console.log("5");  
+        }else{
+            // console.log(element);
+        } 
+    }
+
+
+
+let myArr = [1,2,3,4,5,6,7]
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+    
+    
+}
