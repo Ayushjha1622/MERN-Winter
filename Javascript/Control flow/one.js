faea37770839dec9274bf1cd6e16516d584d2820
Ayush jha <jhaa97204@gmail.com>
2025-01-29 10:33:08 +0530

    new file

---
+++ Javascript/Control flow/one.js
@@ -0,0 +1,13 @@
+const balance = 1000
+if (balance > 500 ) console.log("test") , console.log("test2");
+
+
+if (balance <500) {
+        console.log("less than 500");
+    }else if (balance < 750){
+        console.log("less than 750");
+    }else if (balance < 900 ){
+        console.log("less than 900");
+    }else{
+        console.log("greater than 900");
+    }
