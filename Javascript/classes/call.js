38a2c392dd6f2d31099bcff97b5dadc4e0b8f983
Ayush jha <jhaa97204@gmail.com>
2025-02-02 15:34:56 +0530

    call

---
+++ Javascript/classes/call.js
@@ -0,0 +1,17 @@
+function setUserName(username){
+    this.username = username;
+    console.log("CALLED");
+    
+}
+
+
+function createUser(username,email,password){
+    setUserName.call(this,username)
+
+
+    this.email = email
+    this.password = password
+}
+
+const ch = new createUser("ayuhs","abc@gmail.com",12345)
+console.log(ch);
