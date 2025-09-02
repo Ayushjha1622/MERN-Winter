c2234d301743369a55d18231b33e9140cbcf16fd
Ayush jha <jhaa97204@gmail.com>
2025-09-02 14:48:12 +0530

    new

--- Javascript/Projects/03_Digital_Clock/script.js
+++ Javascript/Projects/03_Digital_Clock/script.js
@@ -1,12 +1,23 @@
-const clock=document.getElementById('clock')
-// const clock=document.querySelector('#clock')
+// const clock=document.getElementById('clock')
+// // const clock=document.querySelector('#clock')
 
 
+
+
+
+// setInterval(function(){
+//     let date = new Date();
+//     //console.log(date.toLocaleTimeString());
+//     clock.innerHTML = date.toLocaleTimeString()
+// },1000)
+
+
+let clock = document.getElementById("clock")
 
 
 
 setInterval(function(){
+    
     let date = new Date();
-    //console.log(date.toLocaleTimeString());
-    clock.innerHTML = date.toLocaleTimeString()
-},1000)
+    clock.innerHTML = date.toLocaleTimeString("clock")
+    }, 1000)
